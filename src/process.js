9a98e40723526af33a5fa0a3d35d9e0487b62b42
Duy Nguyen <duynguyen.dev.it@gmail.com>
2026-04-07 09:22:39 +0000

    Add missing getProcessingStatus import

--- src/process.js
+++ src/process.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { extractText } from './parser.js';
 import { generateSummary, extractConcepts, findConnections } from './llm.js';
-import { updateProcessingStatus, addProcessingLog, resetProcessingStatus } from './status.js';
+import { updateProcessingStatus, addProcessingLog, resetProcessingStatus, getProcessingStatus } from './status.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
